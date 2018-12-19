a7112ab8ebddd03a0da058c09c8ba21937b0eb1f
AnnaDoroshenko <annadoroshenko03@gmail.com>
2018-12-20 00:07:41 +0200

    new classes are added and draw() is updated

--- CourseWork/main.py
+++ CourseWork/main.py
@@ -4,7 +4,7 @@ import tools
 if __name__ == "__main__":
     vertexBunch = []
     linkBunch = []
-    coreBunch = []
+    familyBunch = []
 
     vertexBunch.append(tools.Vertex(0, 3))
     vertexBunch.append(tools.Vertex(1, 4))
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     linkBunch.append(tools.Link(0, 2, 2))
     linkBunch.append(tools.Link(1, 2, 3))
 
-    # for i in range(1, tools.CORE_AMOUNT + 1):
-    #     coreBunch.append(tools.Core(i))
-
-    tools.draw(coreBunch)
+    familyBunch = tools.createFamilies(vertexBunch, linkBunch)
+    print(familyBunch)
+    
+    # tools.draw(tasks, transmissions, coreAmount, totalTime)
--- CourseWork/tools.py
+++ CourseWork/tools.py
@@ -1,7 +1,3 @@
-# TODO: it will be counted later, delete as global variable
-CORE_AMOUNT = 3
-
-
 class Vertex:
     def __init__(self, vertexId, weight):
         self.vertexId = vertexId
@@ -29,6 +25,25 @@ class Family:
         self.readyParents = readyParents
         self.children = children
 
+    def __repr__(self):
+        return "\n Family(id: {}, weight: {}, parents: {}, readyParents: {}, children: {})".format(str(self.taskId), str(self.weight), str(self.parents), str(self.readyParents), str(self.children))
+
+
+class Task:
+    def __init__(self, taskId, start, end, core):
+        self.taskId = taskId
+        self.start = start
+        self.end = end
+        self.core = core
+
+
+class Transmission:
+    def __init__(self, fromV, toV, start, end):
+        self.fromV = fromV
+        self.toV = toV
+        self.start = start
+        self.end = end
+
 
 def createFamilies(vertexBunch, linkBunch):
     # TODO: exception if bunches are empty ???
@@ -42,7 +57,7 @@ def createFamilies(vertexBunch, linkBunch):
         for link in linkBunch:
             if link.child == currentVertexId:
                 currentParents.append((link.parent, link.weight))
-            if link.parents == currentVertexId:
+            if link.parent == currentVertexId:
                 currentChildren.append((link.child, link.weight))
         for _ in range(len(currentParents)):
             currentReadyParents.append(False)
@@ -54,54 +69,48 @@ def createFamilies(vertexBunch, linkBunch):
 def isReadyFamily(family):
     return len(family.readyParents) == family.readyParents.count(True)
 
-# def printGraph():
-#     for v in vertexBunch:
-#         res = ["Vertex"]
-#         res.append(str(v.vertexId))
-#         res.append(":")
-#         for l in linkBunch:
-#             if l.parent == v.vertexId:
-#                 res.append("child -")
-#                 res.append(str(l.child))
-#             if l.child == v.vertexId:
-#                 res.append("parent -")
-#                 res.append(str(l.parent))
-#         res = " ".join(res)
-#         print(res)
+
+def process(familyBunch):
+    coreBunch = []
+    coreAmount = 0
+    tasks = []
+    transmissions = []
+    while len(familyBunch) != len(tasks):
+        for family in familyBunch:
+            if isReadyFamily(family): break
+
+    return tasks, transmissions, coreAmount, totalTime
 
 
-# def processGraph():
-#     while len(vertexBunch) != 0:
-#         for v in vertexBunch:
-#             if v.isReady == True: 
-#
-#         # for proc in processBunch:
-
-
-# TODO: method takes tasks[] and transmissions[]
-def draw(coreBunch):
-    tasks = [{"id" : 1, "start" : 1, "end" : 3, "core" : 1},
-            {"id" : 2, "start" : 1, "end" : 4, "core" : 2},
-            {"id" : 3, "start" : 5, "end" : 6, "core" : 2}]
-    transmissions = [{"from" : 1, "to" : 2, "begin" : 3, "end" : 5}]
+def draw(tasks, transmissions, coreAmount, totalTime):
+    coreAmount = 3
+    totalTime = 6
+    tasks = [Task(1, 1, 3, 1),
+            Task(2, 1, 4, 2),
+            Task(3, 5, 6, 2)]
+    # {"id" : 1, "start" : 1, "end" : 3, "core" : 1},
+    #         {"id" : 2, "start" : 1, "end" : 4, "core" : 2},
+    #         {"id" : 3, "start" : 5, "end" : 6, "core" : 2}]
+    transmissions = [Transmission(1, 2, 3, 5)]
+    # {"from" : 1, "to" : 2, "begin" : 3, "end" : 5}]
     tasks.reverse()
     # transmissions.reverse()
 
-    outputTable = [["     " for j in range(CORE_AMOUNT + 2)] for i in range(7)]
+    outputTable = [["     " for j in range(coreAmount + 2)] for i in range(totalTime + 1)]
     outputTable[0][0] = " Tick"
     # TODO: change coreBunch on amountOfCores, will be simpler
-    for core in coreBunch:
-        outputTable[0][core.coreId] = "Core" + str(core.coreId)
-    outputTable[0][CORE_AMOUNT + 1] = "Transmission"
+    for core in range(coreAmount):
+        outputTable[0][core + 1] = "Core" + str(core + 1)
+    outputTable[0][coreAmount + 1] = "Transmission"
     while len(tasks) != 0:
         task = tasks.pop()
-        for tick in range(task["start"], task["end"] + 1):
+        for tick in range(task.start, task.end + 1):
             outputTable[tick][0] = "   " + str(tick) + " "
-            outputTable[tick][task["core"]] = "  " + str(task["id"]) + "  "
+            outputTable[tick][task.core] = "  " + str(task.taskId) + "  "
     while len(transmissions) != 0:
         transmission = transmissions.pop()
-        outputTable[transmission["begin"]][CORE_AMOUNT + 1] = "   " + str(transmission["from"]) + " >> " + str(transmission["to"])
-        outputTable[transmission["end"]][CORE_AMOUNT + 1] = "   " + str(transmission["to"]) + " << " + str(transmission["from"])
+        outputTable[transmission.start][coreAmount + 1] = "   " + str(transmission.fromV) + " >> " + str(transmission.toV)
+        outputTable[transmission.end][coreAmount + 1] = "   " + str(transmission.toV) + " << " + str(transmission.fromV)
 
     for row in outputTable:
         row = " | ".join(row)
